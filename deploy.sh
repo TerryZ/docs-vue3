3f56f30b657830ada8ad44de848d747681e68095
Terry Zeng <terry5@foxmail.com>
2023-09-08 21:42:28 +0800

    chore: 更新构建脚本

--- deploy.sh
+++ deploy.sh
@@ -10,22 +10,24 @@ pnpm build
 
 # add proxy in git global config
 git config --global http.proxy http://192.168.1.100:10808
-git clone https://terryz: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@github.com/TerryZ/TerryZ.github.io.git
+# git clone https://terryz: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@github.com/TerryZ/TerryZ.github.io.git
+git clone https://terryz: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@github.com/TerryZ/docs-vue3.git
 git checkout master
 
-cd TerryZ.github.io
-rm -rf vue3
-mkdir vue3
+cd docs-vue3
+rm -rf web
+mkdir web
 
 # copy builded file to vue folder
-cp -r ../docs/.vitepress/dist/* vue3
+cp -r ../docs/.vitepress/dist/* web
 
 git config user.email "terry5@foxmail.com"
 git config user.name "TerryZ"
 
 # git commit and push to github
-git add --all vue3
+git add --all web
 git commit -m 'auto commit changes and push'
-git push https://terryz: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@github.com/TerryZ/TerryZ.github.io.git
+# git push https://terryz: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@github.com/TerryZ/TerryZ.github.io.git
+git push https://terryz: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@github.com/TerryZ/docs-vue3.git
 # remove proxy in git global config
 git config --global --unset http.proxy

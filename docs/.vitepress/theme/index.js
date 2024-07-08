34fd991b4bf9edf82589b02d30650a40da372dc5
TerryZ <terry5@foxmail.com>
2024-07-08 11:50:39 +0800

    chore: 更新 vitepress 版本

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,5 @@
-import { watchEffect } from 'vue'
-import { inBrowser, useData } from 'vitepress'
 import DefaultTheme from "vitepress/theme"
+import Layout from './Layout.vue'
 
 // 引用 bootstrap 工具类样式
 // import 'bootstrap/dist/css/bootstrap.min.css'
@@ -10,28 +9,21 @@ import '../../../bootstrap.sass'
 import './component-fix.sass'
 import './custom.sass'
 
+// export default {
+//   ...DefaultTheme,
+//   setup() {
+//     const { lang } = useData()
+//     watchEffect(() => {
+//       // if (inBrowser) {
+//       //   document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2024 00:00:00 UTC; path=/`
+//       // }
+//       if (typeof document !== 'undefined') {
+//         document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2024 00:00:00 UTC; path=/`
+//       }
+//     })
+//   }
+// }
 export default {
-  ...DefaultTheme,
-  // enhanceApp(ctx) {
-  //   DefaultTheme.enhanceApp(ctx)
-  //   console.log(ctx)
-  //   ctx.app.mixin({
-  //     beforeMount () {
-  //       import('v-page').then(module => {
-  //         ctx.app.component('Page', module.Page)
-  //       })
-  //     }
-  //   })
-  // },
-  setup() {
-    const { lang } = useData()
-    watchEffect(() => {
-      // if (inBrowser) {
-      //   document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2024 00:00:00 UTC; path=/`
-      // }
-      if (typeof document !== 'undefined') {
-        document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2024 00:00:00 UTC; path=/`
-      }
-    })
-  }
+  extends: DefaultTheme,
+  Layout
 }
